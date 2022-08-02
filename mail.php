42b90437aee06f3cd33c724c1a49aa33ca4d83ce
Md. Rashidul Islam <92414937+Rashidul-islam-Rashed@users.noreply.github.com>
2022-08-03 04:47:22 +0600

    add all file*

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "rashidul43439@gmail.com";
+$sitename = "Md. Rashidul Islam";
+
+$name = trim($_POST["name"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Name: $name \nEmail: $email \nText: $text";
+
+$pagetitle = "New message from the \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
